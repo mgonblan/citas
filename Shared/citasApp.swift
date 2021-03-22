3f85206d731933885387e9fea8ab9a214b69765f
Manuel Gonzalez <Manuel.gonzalez.freelancer@gmail.com>
2021-03-22 09:04:45 +0100

    Initial Commit

---
+++ Shared/citasApp.swift
@@ -0,0 +1,17 @@
+//
+//  citasApp.swift
+//  Shared
+//
+//  Created by Mañanas on 22/3/21.
+//
+
+import SwiftUI
+
+@main
+struct citasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
